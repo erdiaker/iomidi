33caa18c38ce7337a08925eb6ba928119dd8e176
Erdi Aker <erdiaker@gmail.com>
2014-12-13 16:26:38 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iomidi',
+    version='0.1.0',
+    description='Library for MIDI input and output',
+    author='Erdi Aker',
+    author_email='erdiaker@gmail.com',
+    url='https://github.com/erdiaker/iomidi',
+    packages=['iomidi']
+  )
